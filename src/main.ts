4117701b6c72c35430fe5e8bf60bdd34ca2070ea
Dhanashri Kumbhare <dhanashridilipkumbhare@gmail.com>
2023-12-29 13:48:34 +0530

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { AppModule } from './app/app.module';
+import { applyPolyfills, defineCustomElements } from 'h8k-components/loader';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
